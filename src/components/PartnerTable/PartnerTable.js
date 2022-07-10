318cdf96d971eb20f7ed1566ccd59090b28acd01
vrajesh <vrajeshbarot404@gmail.com>
2022-07-10 22:53:29 +0530

    sorting and pagination added

--- src/components/PartnerTable/PartnerTable.js
+++ src/components/PartnerTable/PartnerTable.js
@@ -1,4 +1,4 @@
-import React, { useSortBy } from 'react';
+import React, {  useState,useEffect,useSortBy } from 'react';
 import * as Element from "./style";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -11,10 +11,53 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
-const PartnerTable = ({user,deletePartner,editPartner}) => {    
+import Pagination from '@mui/material/Pagination';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+const PartnerTable = ({user,deletePartner,editPartner}) => {
+    const [postperpage, setPostperpage] = useState(4);
+    const [startIndex, setStartIndex] = useState(0);
+    const [endIndex, setEndIndex] = useState(postperpage);
+    const [sortedData, setSortedData] = useState(user);
+    const pageNumbers = [];
+    // const handleChangePage = (event, newPage) => {
+    //     setPage(newPage);
+    //   };
+    
+    //   const handleChangeRowsPerPage = event => {
+    //     setRowsPerPage(+event.target.value);
+    //     setPage(0);
+    //   };
+    // const pageNumbers = [];
+    function ascTable() {
+        let ascData = sortedData.sort((a, b) => {
+            if (a.username < b.username) return -1;
+            if (a.username > b.username) return 1;
+            return 0
+        })
+        setSortedData(ascData);
+    }
+    function descTable() {
+        let descData = sortedData.sort((a, b) => {
+            if (a.username < b.username) return 1;
+            if (a.username > b.username) return -1;
+            return 0
+        })
+        setSortedData(descData);
+    }
+    for(let i = 1; i<= Math.ceil(user.length / postperpage); i++) {
+        pageNumbers.push(i);
+    }
+    const paginationIndex = (index) => {
+        console.log(index);
+        setStartIndex(postperpage*(index-1));
+        setEndIndex(postperpage*(index-1) + postperpage);
+    }
     return ( 
         <>
             <Element.Data>
+                <Button variant="outlined" onClick={() => ascTable()}>Asc</Button>
+                <Button variant="outlined" onClick={() => descTable()}>desc</Button>
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
@@ -26,35 +69,46 @@ const PartnerTable = ({user,deletePartner,editPartner}) => {
                         </TableRow>
                         </TableHead>
                         <TableBody>
-                            {user.map((row,index) => (
+                        {sortedData.slice(startIndex,endIndex).map((row,key) => {
+                            return (
                                 <TableRow
-                                key={index}
+                                key={key}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                <TableCell component="th" scope="row">
-                                    {index+1 }
+                            >
+                                <TableCell>
+                                {startIndex + key + 1}
                                 </TableCell>
-                                <TableCell component="th" scope="row">
-                                    {row.name}
+                                <TableCell>
+                                {row.name}
                                 </TableCell>
                                 <TableCell align="right">{row.username}</TableCell>
                                 <TableCell align="right">
                                     <Tooltip title="edit" >
-                                        <IconButton aria-label="edit" size="large" color="secondary" onClick={() => editPartner(index)}>
+                                        <IconButton aria-label="edit" size="large" color="secondary" onClick={() => editPartner(key)}>
                                             <EditIcon />
                                         </IconButton>
                                     </Tooltip>
                                     <Tooltip title="Delete" >
-                                        <IconButton aria-label="delete" size="large" color="error"  onClick={() => deletePartner(index)}>
+                                        <IconButton aria-label="delete" size="large" color="error"  onClick={() => deletePartner(key)}>
                                             <DeleteIcon />
                                         </IconButton>
                                     </Tooltip>
                                 </TableCell>
-                                </TableRow>
-                            ))}
+                            </TableRow>
+                            );
+                        })}
                         </TableBody>
                     </Table>
                 </TableContainer>
+                    <ButtonGroup variant="outlined" aria-label="outlined button group">
+                        {pageNumbers.map(number => {
+                            return (
+                                    <Button key={number} onClick={() => paginationIndex(number)}>
+                                        {number}
+                                    </Button>
+                            );
+                        })}
+                    </ButtonGroup>
             </Element.Data>
         </>
     );
